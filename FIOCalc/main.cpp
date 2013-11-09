3daf7059593c8fbcd086807bd7a1c70a21fa3e99
LukyLuke <lukas@delightsoftware.com>
2013-11-09 15:20:40 +0100

    Corrected formattings

--- FIOCalc/main.cpp
+++ FIOCalc/main.cpp
@@ -33,11 +33,11 @@ map<string, float> sections;
 map<string, float> payments;
 
 int main(int argc, char **argv) {
-  // Floating point output format to standard and not scientific
-  cout.setf(ios::fixed, ios::floatfield);
-  cout.setf(ios::showpoint);
-  cout.precision(2);
-  
+	// Floating point output format to standard and not scientific
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+	
 	createSections();
 	map<string, float> *copy = new map<string, float>();
 	map<string, float>::const_iterator it;
@@ -51,16 +51,16 @@ int main(int argc, char **argv) {
 	
 	while (true) {
 		// copy sections to calculate different paid amounts after and show an overview
-    cout << endl << "Recommendations:" << endl;
-    cout << "-----------------------" << endl;
+		cout << endl << "Recommendations:" << endl;
+		cout << "-----------------------" << endl;
 		for (it = sections.begin(); it != sections.end(); it++) {
 			copy->insert( pair<string, float>(it->first, it->second) );
-      cout << " " << it->first << ":\t\t" << it->second << endl;
+			cout << " " << it->first << ":\t\t" << it->second << endl;
 		}
-    cout << "-----------------------" << endl;
-    cout << " Recommend:\t" << sum << endl;
-    cout << "=======================" << endl << endl;
-    
+		cout << "-----------------------" << endl;
+		cout << " Recommend:\t" << sum << endl;
+		cout << "=======================" << endl << endl;
+		
 		// Get paid amount or exit
 		cout << "Amount paid (Empty to quit): ";
 		getline(cin, line);
@@ -75,23 +75,23 @@ int main(int argc, char **argv) {
 		calculate(copy, amount);
 		
 		cout << endl << "Payments:" << endl;
-    cout << "-----------------------" << endl;
+		cout << "-----------------------" << endl;
 		for (it = payments.begin(); it != payments.end(); it++) {
-      cout << " " << it->first << ":\t\t" << it->second << endl;
+			cout << " " << it->first << ":\t\t" << it->second << endl;
 		}
 		cout << "-----------------------" << endl;
-    cout << " Paid:\t\t" << amount << endl;
+		cout << " Paid:\t\t" << amount << endl;
 		cout << "=======================" << endl << endl;
 	}
 	return 0;
 }
 
 /**
- * Recursive call to calculate all amounts from all sections
- * 
- * @param *copy Outstanding sections
- * @param amount Outstanding amount
- */
+* Recursive call to calculate all amounts from all sections
+* 
+* @param *copy Outstanding sections
+* @param amount Outstanding amount
+*/
 void calculate(map<string, float> *copy, float amount) {
 	if (amount <= 0) {
 		return;
@@ -107,7 +107,7 @@ void calculate(map<string, float> *copy, float amount) {
 			for (it = copy->begin(); it != copy->end(); it++) {
 				payments.insert( pair<string, float>(it->first, amount * (it->second / sum)) );
 			}
-      copy->clear();
+			copy->clear();
 			return;
 		}
 		
@@ -116,7 +116,7 @@ void calculate(map<string, float> *copy, float amount) {
 			smallest = smallest <= 0 || it->second < smallest ? it->second : smallest;
 		}
 		due = amount / copy->size();
-    
+		
 		// if the due is smaller than the smallest amount, give the same amount to each unpaid section
 		if (due <= smallest) {
 			for (it = copy->begin(); it != copy->end(); it++) {
@@ -139,9 +139,9 @@ void calculate(map<string, float> *copy, float amount) {
 }
 
 /**
- * User can input a SectionName and an amount after
- * finished by enter an empty string as Sectionname
- */
+* User can input a SectionName and an amount after
+* finished by enter an empty string as Sectionname
+*/
 void createSections() {
 	string line1, line2;
 	float amount;
